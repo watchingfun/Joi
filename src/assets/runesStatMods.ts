80f6e0aa1efb7b90f91b52735f50ada99c6c4598
watchingfun <wenhuan2333@gmail.com>
2024-02-24 20:15:56 +0800

    更新： 14.2 资源文件

--- src/assets/runesStatMods.ts
+++ src/assets/runesStatMods.ts
@@ -36,37 +36,37 @@ export const runesStatMods: RuneStatMod[][] = [
 			icon: "perk-images/StatMods/StatModsAdaptiveForceIcon.png"
 		},
 		{
-			id: 5002,
-			name: "护甲",
-			desc: "+6 护甲",
-			icon: "perk-images/StatMods/StatModsArmorIcon.png"
+			id: 5010,
+			name: "移动速度",
+			desc: "+2% 移动速度",
+			icon: "perk-images/StatMods/StatModsMovementSpeedIcon.png"
 		},
 		{
-			id: 5003,
-			name: "魔法抗性",
-			desc: "+8 魔法抗性",
-			icon: "perk-images/StatMods/StatModsMagicResIcon.png"
+			id: 5001,
+			name: "成长生命值",
+			desc: "+10-180生命值(基于等级)",
+			icon: "perk-images/StatMods/StatModsHealthPlusIcon.png"
 		}
 	],
 	[
 		{
-			id: 5001,
-			name: "成长生命值",
-			desc: "+15-140生命值(基于等级)",
+			id: 5011,
+			name: "生命值",
+			desc: "+65生命值",
 			icon: "perk-images/StatMods/StatModsHealthScalingIcon.png"
 		},
 		{
-			id: 5002,
-			name: "护甲",
-			desc: "+6 护甲",
-			icon: "perk-images/StatMods/StatModsArmorIcon.png"
+			id: 5013,
+			name: "韧性和减速抗性",
+			desc: "+10%韧性和减速抗性",
+			icon: "perk-images/StatMods/StatModsTenacityIcon.png"
 		},
-		{
-			id: 5003,
-			name: "魔法抗性",
-			desc: "+8 魔法抗性",
-			icon: "perk-images/StatMods/StatModsMagicResIcon.png"
-		}
+    {
+      id: 5001,
+      name: "成长生命值",
+      desc: "+10-180生命值(基于等级)",
+      icon: "perk-images/StatMods/StatModsHealthPlusIcon.png"
+    }
 	]
 ];
 
